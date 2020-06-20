454669e65cd6a059ee3440155fc2fedb6ccae874
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-06-21 00:40:36 +0300

    Added register form control handler

--- public/web/js/custom.js
+++ public/web/js/custom.js
@@ -53,3 +53,39 @@ function controlRequiredInputsAreFilled(array){
 
     return isValid;
 }
+
+$('#register_form').on('submit', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var self = $(this);
+    var url = self.attr('action');
+    var method = self.attr('method');
+    var data = self.serialize();
+    var button = self.find(':submit');
+
+    button.attr('disabled', 'disabled');
+
+    var isValid = true;
+
+    isValid = controlRequiredInputsAreFilled(self.find('.required'));
+
+    if (isValid) {
+        $.ajax({
+            type: method,
+            url: url,
+            data: data,
+            success: function (result) {
+                if (result.success) {
+                    toastr.success('Üyelik başarılı lütfen email gelen kutunuzu kontrol ediniz');
+                    self.find(':input').val('');
+                } else {
+                    toastr.error(result.error.message);
+                }
+                button.removeAttr('disabled');
+            }
+        });
+    } else {
+        button.removeAttr('disabled');
+    }
+});
