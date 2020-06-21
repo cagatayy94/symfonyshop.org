1be0c016be4a30bbcd93589811d5c21ebfaa6323
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-06-22 00:20:13 +0300

    Updated user controller added new functions

--- src/Controller/Web/UserController.php
+++ src/Controller/Web/UserController.php
@@ -43,4 +43,80 @@ class UserController extends AbstractController
             ]);
         }
     }
+
+    /**
+     * @Route("/email-activation", name="email_activation")
+     */
+    public function emailActivationAction(Request $request, UserService $userService)
+    {
+        $email = $request->query->get('email');
+        $activationCode = $request->query->get('activationCode');
+
+        try {
+            $userService->approveEmail($email, $activationCode);
+            $result = [
+                'success' => true,
+                'type' => 1
+            ];
+            
+        } catch (\Exception $e) {
+            $result = [
+                'success' => false,
+                'type' => $e->getMessage()
+            ];
+        }
+
+        return $this->render('Web/Default/email-approve.html.php', [
+            'result' => $result
+        ]);
+    }
+
+    /**
+     * @Route("/new-activation-code", name="new_email_activation_code")
+     */
+    public function sendNewCodeAction(Request $request, UserService $userService)
+    {
+        $email = $request->request->get('email');
+
+        if ($request->isMethod('post')) {
+            $result = true;
+        }else{
+            $result = null;
+        }
+
+        $userService->sendNewCode($email);
+
+        return $this->render('Web/Default/new-activation-code.html.php', [
+            'result' => $result
+        ]);
+    }
+
+    /**
+     * @Route("/email-unsubscribe", name="email_unsubscribe")
+     */
+    public function unsubscribeAction(Request $request, UserService $userService)
+    {
+        $email = $request->query->get('email');
+        $activationCode = $request->query->get('activationCode');
+
+        print_r($email);echo "<pre>";
+
+        die();
+
+
+        try {
+            $userService->create($name, $email, $password, $agreement, $ipAddress, $mobile);
+
+            return new JsonResponse([
+                'success' => true,
+            ]);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ]);
+        }
+    }
 }
