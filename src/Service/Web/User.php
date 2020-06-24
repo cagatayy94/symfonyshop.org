612e4ae11938bad203982a810a27daf04348cd00
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-06-25 01:24:02 +0300

    Created new functions for reset password

--- src/Service/Web/User.php
+++ src/Service/Web/User.php
@@ -317,7 +317,217 @@ class User
         } catch (\Exception $exception) {
             $logFullDetails['details']['exception'] = $exception->getMessage();
             $this->logger->error('Could not resend email approved code', $logFullDetails);
-            throw new \Exception($exception->getMessage());            
+            throw new \Exception("Bir sorun oluştu.");            
+        }
+    }
+
+    /**
+     * Send forgot password email
+     *
+     * @param string $email
+     * @throws \Exception
+     */
+    public function sendForgotPassword($email)
+    {
+        $logDetails = $this->getArguments(__FUNCTION__, func_get_args());
+
+        $logFullDetails = [
+            'entity' => 'User',
+            'activity' => 'sendForgotPassword',
+            'activityId' => 0,
+            'details' => $logDetails
+        ];
+
+        $connection = $this->connection;
+
+        try {
+            if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return;
+            }
+            
+            $user = $connection->executeQuery('
+                    SELECT
+                        ua.id,
+                        ua.name,
+                        ua.activation_code_created_at,
+                        ua.is_email_approved
+                    FROM
+                        user_account ua
+                    WHERE
+                        ua.email = :email
+                    AND
+                        ua.is_deleted = FALSE
+                ', [
+                    'email' => $email,
+                ]
+            )->fetch();
+
+            if (!$user) {
+                return;
+            }
+
+            if ($user['activation_code_created_at']) {
+                $dateDiff = date_diff(new \DateTime(), new \DateTime($user['activation_code_created_at']));
+                $minutes = $dateDiff->days * 24 * 60;
+                $minutes += $dateDiff->h * 60;
+                $minutes += $dateDiff->i;
+
+                if ($minutes < 15) {
+                    return;
+                }
+            }
+
+            $activationCode = $this->uId();
+
+            $statement = $connection->prepare('
+                UPDATE
+                    user_account 
+                SET
+                    activation_code = :activation_code,
+                    activation_code_created_at = NOW()
+                WHERE
+                    id = :id
+            ');
+
+            $statement->bindValue(':id', $user['id']);
+            $statement->bindValue(':activation_code', $activationCode);
+            $statement->execute();
+
+            //send forgot password email
+            $this->mailer->send(
+                $email, 
+                'Parolanızı Sıfırlayın', 
+                'Web/Mail/reset-password.html.php', 
+                [
+                    'name'=> $user['name'],
+                    'email'=> $email,
+                    'activationCode'=> $activationCode,
+                ]);
+
+            $logFullDetails['activityId'] = $user['id'];
+            $this->logger->info('Send reset password email', $logFullDetails);
+        } catch (\InvalidArgumentException $exception) {
+            $logFullDetails['details']['exception'] = $exception->getMessage();
+            $this->logger->error('Could not send reset password email', $logFullDetails);
+            throw $exception;
+        } catch (\Exception $exception) {
+            $logFullDetails['details']['exception'] = $exception->getMessage();
+            $this->logger->error('Could not send reset password email', $logFullDetails);
+            throw new \Exception("Bir sorun oluştu.");            
+        }
+    }
+
+    /**
+     * Control is email and activation code is matching
+     *
+     * @param string $email
+     * @param string $code
+     * @throws \Exception
+     */
+    public function isEmailAndCodeIsMatching($email, $code)
+    {
+        return $this->connection->executeQuery('
+                    SELECT
+                        COUNT(*)
+                    FROM
+                        user_account ua
+                    WHERE
+                        ua.email = :email
+                    AND
+                        ua.is_deleted = FALSE
+                    AND
+                        ua.activation_code = :code
+                ', [
+                    'email' => $email,
+                    'code' => $code,
+                ]
+            )->fetchColumn();
+    }
+
+    /**
+     * Reset forgot password
+     *
+     * @param string $email
+     * @param string $code
+     * @param string $password
+     * @param string $passwordRepeat
+     * @throws \Exception
+     */
+    public function resetForgotPassword($email, $code, $password, $passwordRepeat)
+    {
+        $logDetails = $this->getArguments(__FUNCTION__, func_get_args());
+
+        $logFullDetails = [
+            'entity' => 'User',
+            'activity' => 'resetForgotPassword',
+            'activityId' => 0,
+            'details' => $logDetails
+        ];
+
+        $logDetails['password'] = null;
+        $logDetails['passwordRepeat'] = null;
+
+        $connection = $this->connection;
+
+        try {
+            if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                throw new \InvalidArgumentException("Email hatalı");
+            }
+
+            if ($password != $passwordRepeat) {
+                throw new \InvalidArgumentException("Parolalar uyuşmuyor");
+                
+            }
+
+            $user = $connection->executeQuery('
+                    SELECT
+                        ua.id,
+                        ua.activation_code
+                    FROM
+                        user_account ua
+                    WHERE
+                        ua.email = :email
+                    AND
+                        ua.is_deleted = FALSE
+                ', [
+                    'email' => $email,
+                ]
+            )->fetch();
+
+            if (!$user) {
+                throw new \InvalidArgumentException("Email veya kod hatalı");
+            }
+
+            if ($user['activation_code'] != $code) {
+                throw new \InvalidArgumentException("Email veya kod hatalı");
+            }
+
+            $userEntity = new UserAccount();
+            $encodedPassword = $this->passwordEncoder->encodePassword($userEntity, $password);
+
+            $statement = $connection->prepare('
+                UPDATE
+                    user_account 
+                SET
+                    activation_code = NULL,
+                    password = :password
+                WHERE
+                    id = :id
+            ');
+
+            $statement->bindValue(':id', $user['id']);
+            $statement->bindValue(':password', $encodedPassword);
+            $statement->execute();
+
+            $logFullDetails['activityId'] = $user['id'];
+            $this->logger->info('Reset user password', $logFullDetails);
+        } catch (\InvalidArgumentException $exception) {
+            $logFullDetails['details']['exception'] = $exception->getMessage();
+            $this->logger->error('Could not reset user password', $logFullDetails);
+            throw $exception;
+        } catch (\Exception $exception) {
+            $logFullDetails['details']['exception'] = $exception->getMessage();
+            $this->logger->error('Could not reset user password', $logFullDetails);
             throw new \Exception("Bir sorun oluştu.");            
         }
     }
