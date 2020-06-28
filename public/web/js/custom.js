51bfab43ad85e3e210a26b7508463c3fc2cfe269
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-06-29 01:25:20 +0300

    Added contact form submit handler

--- public/web/js/custom.js
+++ public/web/js/custom.js
@@ -127,3 +127,39 @@ $('#change_password_form').on('submit', function (e) {
         button.removeAttr('disabled');
     }
 });
+
+$('#contact_form').on('submit', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    var self = $(this);
+    var url = self.attr('action');
+    var method = self.attr('method');
+    var data = self.serialize();
+    var button = self.find(':submit');
+
+    button.attr('disabled', 'disabled');
+
+    var isValid = true;
+
+    isValid = controlRequiredInputsAreFilled(self.find('.required'));
+
+    if (isValid) {
+        $.ajax({
+            type: method,
+            url: url,
+            data: data,
+            success: function (result) {
+                if (result.success) {
+                    toastr.success('Bildirim alındı. En yakın zamanda sizinle iletişime geçeceğiz.');
+                    self.find(':input').val('');
+                } else {
+                    toastr.error(result.error.message);
+                }
+                button.removeAttr('disabled');
+            }
+        });
+    } else {
+        button.removeAttr('disabled');
+    }
+});
