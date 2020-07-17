b188dfb5742277ee62609eae42294dcad567cddd
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-07-17 21:25:08 +0300

    Arranged code

--- src/Resources/views/Admin/Profile/list.html.php
+++ src/Resources/views/Admin/Profile/list.html.php
@@ -18,40 +18,40 @@
 			<div class="col-md-12">
 				<div class="box">
 					<div class="box-header">
-							<div class="row">
-									<div class="col-md-10"><h3 class="box-title">Yönetici Profil Listesi</h3></div>
-									<div class="col-md-2">
-										<?php if ($admin->hasRole('profile_create')):?>
-											<button type="button" data-toggle="modal" data-target="#add-new-admin-modal" class="btn btn-block btn-info btn-flat"><i class="fa fa-fw fa-plus"></i> Yeni Ekle </button>
-										<?php endif; ?>
-									</div>
+						<div class="row">
+							<div class="col-md-10"><h3 class="box-title">Yönetici Profil Listesi</h3></div>
+							<div class="col-md-2">
+								<?php if ($admin->hasRole('profile_create')):?>
+									<button type="button" data-toggle="modal" data-target="#add-new-admin-modal" class="btn btn-block btn-info btn-flat"><i class="fa fa-fw fa-plus"></i> Yeni Ekle </button>
+								<?php endif; ?>
 							</div>
+						</div>
 					</div>
 					<!-- /.box-header -->
 					<div class="box-body no-padding">
 						<table class="table table-striped">
 							<tbody>
 							<tr>
-									<th>İsim</th>
-									<th>Kişiler</th>
-									<th></th>
+								<th>İsim</th>
+								<th>Kişiler</th>
+								<th></th>
 							</tr>
 							<?php if (count($adminProfileList)):?>
 								<?php foreach ($adminProfileList as $profileKey => $profilelist): ?>
-										<tr>
-											<td><?php echo $profilelist['name']; ?></td>
-											<td>
-												<?php if (isset($profilelist['admins'])):?>
-													<?php foreach ($profilelist['admins'] as $adminKey => $adminName): ?>
-														 <a href="<?php echo $this->get('router')->path('admin_account_detail', ['adminId' => $adminKey]) ?>"><?php echo $adminName; ?></a><br>
-													<?php endforeach ?>
-												<?php endif; ?>
-											</td>
-											<td>
-												<?php if ($admin->hasRole('profile_detail_show')): ?>
-													<a style="padding: 0;" href="<?php echo $this->get('router')->path('admin_profile_detail', ['profileId' => $profileKey]) ?>" class="btn"><i class="fa fa-edit"></i> Detay </a>
-												<?php endif ?>
-											</td>
+									<tr>
+										<td><?php echo $profilelist['name']; ?></td>
+										<td>
+											<?php if (isset($profilelist['admins'])):?>
+												<?php foreach ($profilelist['admins'] as $adminKey => $adminName): ?>
+													 <a href="<?php echo $this->get('router')->path('admin_account_detail', ['adminId' => $adminKey]) ?>"><?php echo $adminName; ?></a><br>
+												<?php endforeach ?>
+											<?php endif; ?>
+										</td>
+										<td>
+											<?php if ($admin->hasRole('profile_detail_show')): ?>
+												<a style="padding: 0;" href="<?php echo $this->get('router')->path('admin_profile_detail', ['profileId' => $profileKey]) ?>" class="btn"><i class="fa fa-edit"></i> Detay </a>
+											<?php endif ?>
+										</td>
 									</tr>
 								<?php endforeach ?>
 							<?php endif; ?>
