a6bfc8b60132c35d6580f3218165f6d88463e93c
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-08-20 01:31:48 +0300

    Updated product service

--- src/Service/Web/Product.php
+++ src/Service/Web/Product.php
@@ -12,35 +12,167 @@ class Product
 {
     use ServiceTrait;
 
-    public function getAll()
+    /**
+     * Get All Products
+     *
+     * @param int $currentPage
+     * @param int $pageCount
+     * @param int $perPage
+     * @param string $name
+     * @param int $productId
+     * @param date $createdAtStart
+     * @param date $createdAtEnd
+     *
+     * @throws \Exception
+     */
+    public function getAll($currentPage, $pageCount, $perPage, $order, $menuId = null, $categoryId = null, $search = null, $maxPrice = null, $minPrice = null)
     {
         $connection = $this->connection;
 
-        $sql = "
+        //values sql
+        $recordsSql = "
             SELECT
-                *
+                p.id,
+                p.name,
+                p.price,
+                json_agg(pp.path) photo,
+                json_agg(pc.category_id)
             FROM
                 product p
             LEFT JOIN
-                product_photo pp ON pp.id = (
-                                                SELECT
-                                                    id
-                                                FROM
-                                                    product_photo pp
-                                                WHERE
-                                                    pp.is_deleted = FALSE AND pp.product_id =p.id
-                                                LIMIT 1
-                                            )
+                product_category pc on p.id = pc.product_id
+            LEFT JOIN
+                product_photo pp on p.id = pp.product_id
+            LEFT JOIN
+                category_menu cm on pc.category_id = cm.category_id
             WHERE
-                p.is_deleted = FALSE
+                p.is_deleted = false";
 
-        ";
+        if ($categoryId) {
+            $recordsSql.="
+                AND
+                    pc.category_id = :category_id";
+        }
 
-        $statement = $connection->prepare($sql);
+        if ($menuId) {
+            $recordsSql.="
+                AND
+                    cm.menu_id = :menu_id";
+        }
 
+        if ($search) {
+            $recordsSql.="
+                AND
+                    LOWER(p.name) LIKE LOWER(:search) 
+                    OR
+                    LOWER(p.description) LIKE (:search)";
+        }
+
+        $recordsSql.="
+            GROUP BY
+                p.id";
+
+        if ($order == 'price') {
+            $recordsSql .= "
+                ORDER BY
+                    p.price";
+        }elseif($order == 'price-desc'){
+            $recordsSql .= "
+                ORDER BY
+                    p.price DESC ";
+        }else{
+            $recordsSql .= "
+                ORDER BY
+                    p.created_at";
+        }
+
+        // limit sql
+        $recordsSql .= '
+            LIMIT
+                '. $perPage . '
+            OFFSET
+                ' . $perPage * ($currentPage - 1) . '
+        ';
+
+        $statement = $connection->prepare($recordsSql);
+
+        if ($categoryId) {
+            $statement->bindValue('category_id', $categoryId);
+        }
+
+        if ($menuId) {
+            $statement->bindValue('menu_id', $menuId);
+        }
+
+        if ($search) {
+            $statement->bindValue('search', '%'.$search.'%');
+        }
+        
         $statement->execute();
 
-        return $statement->fetchAll();
+        $records = $statement->fetchAll();
+
+        //total sql
+        $totalSql = "
+            SELECT
+                count(p.id)
+            FROM
+                product p
+            LEFT JOIN
+                product_category pc on p.id = pc.product_id
+            LEFT JOIN
+                product_photo pp on p.id = pp.product_id
+            LEFT JOIN
+                category_menu cm on pc.category_id = cm.category_id
+            WHERE
+                p.is_deleted = false";
+
+        if ($categoryId) {
+            $totalSql.="
+                AND
+                    pc.category_id = :category_id";
+        }
+
+        if ($menuId) {
+            $totalSql.="
+                AND
+                    cm.menu_id = :menu_id";
+        }
+
+        if ($search) {
+            $totalSql.="
+                AND
+                    LOWER(p.name) LIKE LOWER(:search) 
+                    OR
+                    LOWER(p.description) LIKE (:search)";
+        }
+
+        $totalSql.="
+            GROUP BY
+                p.id "; 
+
+        $statement = $connection->prepare($totalSql);
+
+        if ($categoryId) {
+            $statement->bindValue('category_id', $categoryId);
+        }
+
+        if ($menuId) {
+            $statement->bindValue('menu_id', $menuId);
+        }
+
+        if ($search) {
+            $statement->bindValue('search', '%'.$search.'%');
+        }
+        
+        $statement->execute();
+
+        $total = $statement->fetchAll();
+
+        return [
+            'total' => count($total),
+            'records' => $records,
+        ];
     }
 
     public function getMaxPrice()
