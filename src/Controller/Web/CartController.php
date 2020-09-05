a5cbe3633e122f6a6241e8819c50f7e3909165fb
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-09-05 17:17:13 +0300

    Created cart controller

---
+++ src/Controller/Web/CartController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Service\Web\Cart as CartService;
+use App\Service\Web\SiteSettings as SiteSettings;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add", name="cart_add")
+     */
+    public function addCartAction(Request $request, CartService $cartService)
+    {
+        $productId = $request->request->get('productId');
+        $variantId = $request->request->get('variant');
+        $user = $this->getUser();
+
+        try {
+            $cartService->addCart($productId, $variantId, $user->getId());
+
+            return new JsonResponse([
+                'success' => true,
+            ]);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/cart/get-total-and-quantity", name="cart_get_total_and_quantity")
+     */
+    public function getCartTotalAndQuantityAction(Request $request, CartService $cartService)
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return new JsonResponse([
+                'success' => true,
+                'data' => [
+                    [
+                        'sum' => "0.00 ₺",
+                        'count' => 0
+                    ]
+                ]
+            ]);
+        }
+        try {
+            $cart = $cartService->getCartTotalAndQuantity($user->getId());
+
+            return new JsonResponse([
+                'success' => true,
+                'data' => $cart
+            ]);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/cart/detail", name="cart_detail")
+     */
+    public function cartDetailAction(Request $request, CartService $cartService)
+    {
+        $user = $this->getUser();
+
+        return $this->render('Web/Cart/cart_detail.html.php', 
+            [
+                'user' => $user
+            ]
+        );
+    }
+
+    /**
+     * @Route("/cart/get-data", name="cart_get_data")
+     */
+    public function getCartData(Request $request, CartService $cartService)
+    {
+        $user = $this->getUser();
+
+        try {
+            $cart = $cartService->getCart($user->getId());
+
+            return new JsonResponse([
+                'success' => true,
+                'data' => $cart
+            ]);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/cart/remove-row", name="cart_remove_row")
+     */
+    public function cartRemoveItemAction(Request $request, CartService $cartService)
+    {
+        $user = $this->getUser();
+        $cartId = $request->request->get('cartId');
+
+        try {
+            $cartService->remove($user->getId(), $cartId);
+
+            return new JsonResponse([
+                'success' => true,
+            ]);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ]);
+        }
+    }
+
+    /**
+     * @Route("/cart/update-quantity", name="cart_update_quantity")
+     */
+    public function cartUpdateQuantityAction(Request $request, CartService $cartService)
+    {
+        $user = $this->getUser();
+
+        $cartId = $request->request->get('cartId');
+        $type = $request->request->get('type');
+
+        try {
+
+            $res = $cartService->updateQuantity($user->getId(), $cartId, $type);
+
+            return new JsonResponse([
+                'success' => true,
+            ]);
+        } catch (\Exception $exception) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => [
+                    'message' => $exception->getMessage()
+                ]
+            ]);
+        }
+    }
+}
