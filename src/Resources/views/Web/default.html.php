4e994e3fefd82c34131e60d16fc209c18c96fb70
Mustafa Çağatay Yılmaz <cagatay.yilmaz@oto.net>
2020-09-14 23:14:33 +0300

    Added mask library

--- src/Resources/views/Web/default.html.php
+++ src/Resources/views/Web/default.html.php
@@ -70,18 +70,7 @@
 <script src="/web/js/examples/examples.gallery.js"></script>
 <!-- toastr js -->
 <script src="/toastr/toastr.min.js"></script>
+<script src="/web/js/jquery.mask.min.js"></script>
 <?php $view['slots']->output('js'); ?>
-
-<!-- Google Analytics: Change UA-XXXXX-X to be your site's ID. Go to http://www.google.com/analytics/ for more information.
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-12345678-1', 'auto');
-    ga('send', 'pageview');
-</script>
- -->
 </body>
 </html>
